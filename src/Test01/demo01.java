e6b38d53b6ba5e50d5eee220eb5f5258ebb66d6e
yc <743195254@qq.com>
2017-12-08 10:40:42 +0800

    moyachao

---
+++ src/Test01/demo01.java
@@ -0,0 +1,9 @@
+package Test01;
+
+public class demo01 {
+	public static void main(String[] args) {
+		
+		int a = 10, b = 4;
+		System.out.println(a / b);
+	}
+}
---
+++ src/Test01/demo02.java
@@ -0,0 +1,15 @@
+package Test01;
+
+public class demo02 {
+	public static void main(String[] args) {
+		
+		
+		String s="kanga";
+		s="kanga"+"roo";         
+		int[]arr ={1,4,9,25};
+		arr[3]=16;
+		arr=new int[4];
+		System.out.println(s);
+		s=null;
+	}
+}
